c9ee9dd8144822999eea55c9b6f6b07ba55ccbcd
Sony Shrestha <sony.sth8@gmail.com>
2022-04-03 14:26:08 +0545

    First Exercise completed

---
+++ Exercise1/Exercise1/Controllers/CustomerController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Exercise1.Models;
+
+namespace Exercise1.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var customer = new List<Customer> {
+                new Customer{Id=1,Name="Customer 1"},
+                new Customer{Id=2,Name="Customer 2"}
+            };
+        
+            return View(customer);
+        }
+    }
+}
---
+++ Exercise1/Exercise1/Controllers/MovieController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Exercise1.Models;
+
+namespace Exercise1.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Index()
+        {
+            var movie = GetMovieList();
+            return View(movie);
+                       
+        }
+
+        public ActionResult MovieSpecificView(int Id)
+        {
+            var customer = GetMovieList().SingleOrDefault(c => c.Id == Id);
+            if (customer == null)
+                return HttpNotFound();
+            else
+                return View(customer);
+        }
+
+        public List<Movie> GetMovieList()
+        {
+            return new List<Movie> {
+                new Movie{Id=1,Name="Movie 1"},
+                new Movie{Id=2,Name="Movie 2"}
+            };
+        }
+    }
+}
+ 
